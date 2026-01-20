8a51137cf7c83114a2e1c485bb9d73e1a1f0bd86
Kagol <kagol@sina.com>
2026-01-20 19:46:13 +0800

    feat: add TinyEditor

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <DemoEditorBasic />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import DemoEditorBasic from './components/DemoEditorBasic.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    DemoEditorBasic
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import '@opentiny/fluent-editor/style.css';
 </style>
---
+++ src/components/DemoEditorBasic.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="editor">
+    <p>Hello TinyEditor!</p>
+  </div>
+</template>
+
+<script>
+  import FluentEditor from '@opentiny/fluent-editor'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted() {
+    // 执行初始化时，请确保能获取到 DOM 元素，如果是在 Vue 项目中，需要在 onMounted 事件中执行。
+    const editor = new FluentEditor('#editor', {
+      theme: 'snow',
+    })
+    console.log('editor', editor);
+    
+  }
+}
+</script>
